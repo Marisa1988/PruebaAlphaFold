083d78d2f6d27f9a6ab27c065537d58486635b8d
Marisa Morales <moralesmurallesm@ufl.edu>
2025-01-22 10:03:48 -0500

    update code

--- alphafold.sh
+++ alphafold.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=8
 #SBATCH --gpus=1
 #SBATCH --mem=300gb
-#SBATCH --time=96:00:00
+#SBATCH --time=168:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=moralesmurallesm@ufl.edu
 
@@ -18,11 +18,9 @@ module load alphafold/2.3.1
 
 
 alphafold_full_db.sh \
-    --fasta_paths=/blue/hmedeiros/moralesmurallesm/PruebaAlphaFold/rcsb_pdb_1GAL.fasta \
-    --output_dir=~/blue/hmedeiros/moralesmurallesm/PruebaAlphaFold/results \
+    --fasta_paths=/blue/hmedeiros/moralesmurallesm/PruebaAlphaFold/3TGLM3.fasta,/blue/hmedeiros/moralesmurallesm/PruebaAlphaFold/3TGLM4.fasta,/blue/hmedeiros/moralesmurallesm/PruebaAlphaFold/3TGLM5.fasta,/blue/hmedeiros/moralesmurallesm/PruebaAlphaFold/3TGLM6.fasta,/blue/hmedeiros/moralesmurallesm/PruebaAlphaFold/3TGLM7.fasta \
+    --output_dir=/blue/hmedeiros/moralesmurallesm/PruebaAlphaFold/results \
     --max_template_date=2020-05-14 \
     --use_gpu_relax=1
 
 date
-
-
